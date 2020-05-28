c30266836887a818c4c695db0ab8edb50571b51c
Khang <khang06@users.noreply.github.com>
2020-05-28 17:32:13 -0400

    fix the fix

--- Chikara/Renderer.cpp
+++ Chikara/Renderer.cpp
@@ -1402,9 +1402,9 @@ void Renderer::drawFrame(float time)
         notes_hidden[i]++;
       } else {
         size_t start = max(0, (n.start - time) / pre_time * NOTE_DEPTH_BUFFER_SIZE);
-        size_t end = min(NOTE_DEPTH_BUFFER_SIZE, (size_t)max(start, ((n.end - time) / pre_time * NOTE_DEPTH_BUFFER_SIZE)));
+        size_t end = min(NOTE_DEPTH_BUFFER_SIZE, max(start, ((n.end - time) / pre_time * NOTE_DEPTH_BUFFER_SIZE)));
         n.hidden = false;
-        if (start >= end) // even after that casting hell it's not enough
+        if (start > end) // even after that casting hell it's not enough
           return;
         // fast path, will probably save a lot of time
         if (!(depth_buf[start] && depth_buf[end])) {
